362091079206dcd62e304bd8377244a3e5d3c1de
ArtemUnreal71 <artemonych628@gmail.com>
2024-07-20 14:04:39 +0200

    A

--- CZ/HeaderLoad.js
+++
@@ -1,9 +0,0 @@
-function loadHeader() {
-    fetch('/CZ/Header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('header').innerHTML = data;
-        });
-}
-
-document.addEventListener('DOMContentLoaded', loadHeader);
