2d0379672e91849cb746563a667ac6a0a3ea5a7e
ArtemUnreal71 <artemonych628@gmail.com>
2024-09-07 19:57:16 +0200

    Authentication fix

--- CZ/Jobs/Fibretel/Fibretel.js
+++ CZ/Jobs/Fibretel/Fibretel.js
@@ -1,5 +1,9 @@
 const images = document.querySelectorAll('img');
 const dialog = document.querySelector('dialog');
+
+if(!sessionStorage.getItem('logged'))
+    window.location.href = '../../../index.html';
+
 images.forEach(i => {
     i.addEventListener('click', c=> {
         console.log('opening');
