df3a4891f889d2b43845093c535fd4ad63008003
ArtemUnreal71 <artemonych628@gmail.com>
2024-10-17 15:24:21 +0200

    Floating Button
    Added animation and tooltip

--- CZ/Home/Home.js
+++ CZ/Home/Home.js
@@ -1,34 +1,36 @@
-// const download = document.querySelector('.downloadButton');
-//
-// loadHtml2PdfLibrary();
-//
-// download.addEventListener('click', c => {
-//     if (window.innerWidth < 1440) {
-//         alert('Stahování je povoleno pouze z počítače nebo laptopu');
-//     } else {
-//         window.scrollTo({top: 0,});
-//         downloadPDF();
-//     }
-// })
-//
-// function loadHtml2PdfLibrary() {
-//     const script = document.createElement('script');
-//     script.src = "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js";
-//     document.head.appendChild(script);
-// }
-//
-// function downloadPDF() {
-//     var main = document.querySelector('main');
-//
-//     html2pdf(main, {
-//         margin: 0,
-//         filename: 'Zacharcenko_Artem-zivotopis.pdf',
-//         image: {type: 'png', quality: 0.5},
-//         html2canvas: {scale: 0.98},
-//         jsPDF: {
-//             unit: 'cm',
-//             format: [main.offsetWidth * 2.54 / 96, (main.offsetHeight + 100) * 2.54 / 96],
-//             orientation: 'landscape'
-//         }
-//     });
-// }
+const button = document.querySelector('#downloadButton');
+const tooltip = document.querySelector('#downloadButtonTooltip');
+let tooltipOpacityMax = false;
+let buttonShakeSet = false;
+let buttonInterval = null;
+
+const tooltipInterval = setInterval(() => {
+    if (tooltipOpacityMax)
+        tooltip.style.opacity = 0.1;
+    else
+        tooltip.style.opacity = 1;
+
+    tooltipOpacityMax = !tooltipOpacityMax;
+}, 1500)
+
+button.addEventListener('mouseover', (e) => {
+    if (!buttonShakeSet) {
+        tooltip.style.visibility = 'hidden';
+        clearInterval(tooltipInterval);
+        buttonInterval = setInterval(() => {
+            button.classList.toggle('buttonShake');
+        }, 7500)
+        buttonShakeSet = true;
+    }
+})
+
+button.addEventListener('click', (e) => {
+    if (buttonInterval) {
+        clearInterval(buttonInterval);
+        const invisibleTimeout = setTimeout(() => {
+            button.style.visibility = 'hidden';
+            clearTimeout(invisibleTimeout);
+        }, 1500)
+
+    }
+})
