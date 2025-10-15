bfc756a7fd3d8b319e35c966c58de26a42f8f940
Artem Z. <107121930+ArtemUnreal71@users.noreply.github.com>
2025-10-15 17:04:07 +0200

    feat: Security Removed & New Web Link

--- CZ/Jobs/Loop/Loop.js
+++ CZ/Jobs/Loop/Loop.js
@@ -1,13 +1,10 @@
-const images = document.querySelectorAll('img');
-const dialog = document.querySelector('dialog');
+const images = document.querySelectorAll("img");
+const dialog = document.querySelector("dialog");
 
-if(!sessionStorage.getItem('logged'))
-    window.location.href = '../../../index.html';
-
-images.forEach(i => {
-    i.addEventListener('click', c=> {
-        console.log('opening');
-        dialog.querySelector('img').src = i.getAttribute('src');
-        dialog.showModal();
-    })
-})
+images.forEach((i) => {
+  i.addEventListener("click", (c) => {
+    console.log("opening");
+    dialog.querySelector("img").src = i.getAttribute("src");
+    dialog.showModal();
+  });
+});
